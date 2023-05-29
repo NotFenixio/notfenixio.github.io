72b831ce01960573cec85d3b8ea7ed1dd6328814
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2023-05-29 15:15:21 +0200

    Create countdown.js

---
+++ utils/countdown.js
@@ -0,0 +1,30 @@
+let counter = 60;
+var date = new Date("March, 2023 16:30:00").getTime();
+let Cdays = document.getElementById('days');
+let Chours = document.getElementById('hours');
+let Cminutes = document.getElementById('minutes');
+let Cseconds = document.getElementById('seconds');
+var x = setInterval(function() {
+
+  // get today's time
+  var now = new Date().getTime();
+
+  // finding distance
+  var distance = date - now;
+
+  // maths for the cells
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // display result in all the cells
+  Cdays.style.setProperty("--value", days);
+	Chours.style.setProperty("--value", hours);
+	Cminutes.style.setProperty("--value", minutes);
+	Cseconds.style.setProperty("--value", seconds);
+  // If finished, clear the interval so it doesn't show negative values
+  if (distance < 0) {
+    clearInterval(x);
+  }
+}, 1000);
