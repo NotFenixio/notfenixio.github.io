f8ad3f353da1f02c511ac76d5ce6aa423fe01c69
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2024-01-31 08:08:48 +0100

    You shall suffer the consequences of your actions

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,5 +1,5 @@
 let counter = 60;
-var date = new Date("March 21, 2024 13:00:00").getTime();
+var date = new Date("March 22, 2024 13:00:00").getTime();
 let Cdays = document.getElementById('days');
 let Chours = document.getElementById('hours');
 let Cminutes = document.getElementById('minutes');
