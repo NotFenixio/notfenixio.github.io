ffae0546ceb444559f344616578bb5b2fe7f2c37
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2024-08-14 13:38:37 +0200

    nuke time!

--- onecommand/ubuntu/redis.sh
+++
@@ -1,9 +0,0 @@
-#mostly taken from https://redis.io/docs/latest/operate/oss_and_stack/install/install-redis/install-redis-on-linux/
-sudo apt install lsb-release curl gpg
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-sudo apt-get update
-sudo apt-get install redis
-redis-server
